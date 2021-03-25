d854e12959bf99797e5ac15c95b0ca830f0e0af9
lucasdiazcano <lucasdiazcano@hotmail.com>
2021-03-25 17:01:55 -0300

    Comienzo del formulario / estilos

---
+++ src/components/Form/CarouselForm.jsx
@@ -0,0 +1,17 @@
+import { Button } from 'react-bootstrap';
+import React from 'react'
+
+// Este sería un carousel que mostraría imágenes del impacto que tiene la donación que está haciendo el donante.
+
+const CarouselForm = () => {
+
+    return (
+        <div>
+           <h2>¿Que impacto
+                        tiene tu donación? </h2>
+        </div>
+    )
+}
+
+export default CarouselForm
+
