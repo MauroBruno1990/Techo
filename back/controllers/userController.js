f05185de5c3af2b3022cbbe6a3bbd2b5394afbc6
MarCortina <marmica24@hotmail.com>
2021-03-30 17:00:47 -0300

    post

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,23 +1,32 @@
 const User = require("../dbmodels/user");
 
 const userController = {
-  create(req, res) { 
-    // buscar si existe el usuario por ID
-    User.create(req.body)
-      .then((user) => {
-        res.status(201).send(user);
-      })
-      .catch((err) => {
-        res.status(500).send(err);
-      });
-  },
-  update(req, res) {
-    User.findByIdAndUpdate({ _id: req.params.id }, req.body)
-      .then((user) => res.send(user).status(200))
-      .catch((e) => res.send(e).status(500));
-  },
-};
 
+  create(req, res) { 
+    User.findOne({email: req.body.email})    //buscamos el usuario por el mail y nos trae toda la data
+    .then((data)=>{
+if(data){                                     // si hay usuario creado lo updatea
+  User.findOneAndUpdate({ email: data.email }, req.body)
+      .then((userMail) => res.send(userMail).status(200))
+      .catch((e) => res.send(e).status(500));
+  }else{                                           // sino hay usuario creado lo crea 
+    User.create(req.body)
+    .then((user) => {
+      res.status(201).send(user);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    })
+
+  }
+    
+})
+  },update(req, res) {
+    User.findOneAndUpdate({ email: req.body.email }, req.body)
+    .then((userMail) => res.send(userMail).status(200))
+    .catch((e) => res.send(e).status(500));
+}
+};
 /// crear update para modificar datos de un usuario ya registrado
 
 
