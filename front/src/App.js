9b63603eac1dc6c0119c4f4a0481c1fc4b9e5204
MarCortina <marmica24@hotmail.com>
2021-04-07 17:54:13 -0300

    Componente Reality

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,9 @@
 import Navbar from "./components/Navbar/Navbar";
 import MasterForm from "./components/Form/MasterForm";
 import Footer from "./components/Footer/Footer";
-import SinglePayment from "./components/SinglePayment/SinglePayment";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Reality from "./components/Reality/Reality";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
         </Switch>
         <Redirect from="/" to="/Techo" />
       </BrowserRouter>
-
+      <Reality />
       <Footer />
     </div>
   );
